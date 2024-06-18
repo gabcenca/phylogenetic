9dc0637413f90f9024910ed8baf1e0f4a46fcec0
gabcenca <gabrielacentenoca@gmail.com>
2024-06-18 15:01:25 -0600

    Agregar comandos para BIEN

--- prueba_datos.R
+++ prueba_datos.R
@@ -1,1 +1,22 @@
-#Agregar base de datos
+#Setup
+library(BIEN)
+library(ape) #Package for working with phylogenies in R
+library(maps) #Useful for making quick maps of occurrences
+library(sf) # A package for spatial data
+
+#Occurrence records for a species
+#Quercus_rugosa <- BIEN_occurrence_species(species = "Quercus rugosa",
+                                          cultivated = TRUE,
+                                          all.taxonomy = TRUE,
+                                          natives.only = TRUE, 
+                                          observation.type = TRUE,
+                                          political.boundaries = TRUE)
+#str(Quercus_rugosa)
+#head(Quercus_rugosa)
+
+
+
+#Phylogenies
+phylo <- BIEN_phylogeny_conservative()
+plot.phylo(x = phylo, show.tip.label =  FALSE)
+phylo_species <- phylo$tip.label
