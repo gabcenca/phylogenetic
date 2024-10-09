94e43afc9eba960c060ab7c4b3bf8f15b6cead0a
gabcenca <gabrielacentenoca@gmail.com>
2024-10-09 09:11:15 -0600

    agregue un resumen de los datos con glimpse y skim. Primeros esbozos de tratar de obtener el nombre cientifico

--- bin/scripts/cleanData.R
+++ bin/scripts/cleanData.R
@@ -1,5 +1,21 @@
+library(readr)
+library(dplyr)
+library(skimr)
+library(rgbif)
+
+
+herbGbifBien <- read_csv("data/temp/herbaGbifBIEN_df/herbGbifBien.csv")
+
+#Ver un resumen de mis datos
 glimpse(herbGbifBien)
 head(herbGbifBien)
-
-library(skimr)
 skim(herbGbifBien)
+
+# Extraer los nombres científicos únicos
+species_names <- unique(herbGbifBien$scientificName)
+
+# Validar los nombres científicos en GBIF
+validated_species <- lapply(species_names, function(x) name_backbone(name = x))
+
+# Mostrar los resultados
+validated_species
