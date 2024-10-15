35ca892892ebb1a892c5ec6929fb4746c2f9860a
gabcenca <gabrielacentenoca@gmail.com>
2024-10-15 14:59:17 -0600

    anadi carpeta de utaxonstand

---
+++ taxonStand/scripto_utaxonstand.R
@@ -0,0 +1,23 @@
+library(U.Taxonstand)
+library(openxlsx)
+library(dplyr)
+
+
+splist <- read.xlsx("spExample.xlsx")
+databaseExample <- read.xlsx("databaseExample.xlsx")
+#splist<- rename(splist, "SORTER"="X1")
+
+# The input as a dataframe with the columns "SPECIES", "AUTHOR" and/or "RANK"
+
+res <- nameMatch(spList=splist, spSource=databaseExample, author = TRUE, max.distance= 1)
+
+
+head(res)
+
+write.csv(res,file="nameMatch_taxonstand.csv")
+
+
+# The current default only keeps the first 'best' matching result for each taxon name. If you want to check all the matched results, please change the option 'matchFirst=FALSE'.
+res <- nameMatch(spList=splist, spSource=databaseExample, author = TRUE, max.distance= 1, matchFirst=FALSE)
+dim(res)
+
