b37be0186292a465d3de223dc4636fd41188a254
gabcenca <gabrielacentenoca@gmail.com>
2025-04-29 12:09:38 -0600

    anadi informacion de descarga de gbif

--- bin/1_downloadData.R
+++ bin/1_downloadData.R
@@ -30,6 +30,22 @@ gbif_quercus_mex <- occ_download(
 
 occ_download_meta(gbif_quercus_mex)
 
+# occ_download_meta(gbif_quercus_mex)
+# <<gbif download metadata>>
+# Status: SUCCEEDED
+# DOI: 10.15468/dl.bzhg67
+# Format: DWCA
+# Download key: 0005520-250426092105405
+# Created: 2025-04-29T16:34:53.239+00:00
+# Modified: 2025-04-29T16:48:44.398+00:00
+# Download link: https://api.gbif.org/v1/occurrence/download/request/0005520-250426092105405.zip
+# Total records: 70558
+
+# gbif_citation("0005520-250426092105405")
+# $download
+# [1] "GBIF Occurrence Download https://doi.org/10.15468/dl.bzhg67 Accessed from R via rgbif (https://github.com/ropensci/rgbif) on 2025-04-29"
+
+
 #Then you will import the data
 records_gbif_dwc <- occ_download_get(gbif_quercus_mex, 
                                      path = here::here("data/in/gbif_dwc/"),
